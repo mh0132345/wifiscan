d2b8b3c5428f4f171baa6f66990f1dbd1c287da6
Hoang <mh0132345@gmail.com>
2017-03-31 11:36:16 +0000

    Update

---
+++ screen.h
@@ -0,0 +1,12 @@
+// includes the function prototypes for screen manipulation
+enum COLOR{BLACK=30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
+#define UNICODE
+#define BAR "/u2590" // /u2590 for bar
+
+// function prototypes
+void display_channel(void);
+void setFGcolor(int fq);
+void resetColors(void);
+void gotoXY(int row, int col);
+void displayBar(double rms, int col);
+void clearScreen(void);
